944d7aebf99bbf39c9fcfb6e1ea26bb06192b95c
Petr Pavlik <pavlipe7@gmail.com>
2012-12-29 13:04:51 +0100

    example code comments

--- PPLabel/ViewController.m
+++ PPLabel/ViewController.m
@@ -51,39 +51,43 @@
 - (void)highlightWordContainingCharacterAtIndex:(CFIndex)charIndex {
     
     if (charIndex==NSNotFound) {
+        
+        //user did nat click on any word
         [self removeHighlight];
         return;
     }
     
     NSString* string = self.label.text;
     
+    //compute the positions of space characters next to the charIndex
     NSRange end = [string rangeOfString:@" " options:0 range:NSMakeRange(charIndex, string.length - charIndex)];
     NSRange front = [string rangeOfString:@" " options:NSBackwardsSearch range:NSMakeRange(0, charIndex)];
     
     if (front.location == NSNotFound) {
-        front.location = 0;
+        front.location = 0; //first word was selected
     }
     
     if (end.location == NSNotFound) {
-        end.location = string.length-1;
+        end.location = string.length-1; //last word was selected
     }
     
     NSRange wordRange = NSMakeRange(front.location, end.location-front.location);
     
-    if (front.location!=0) {
+    if (front.location!=0) { //fix trimming
         wordRange.location += 1;
         wordRange.length -= 1;
     }
     
     if (wordRange.location == self.highlightedRange.location) {
-        return;
+        return; //this word is already highlighted
     }
     else {
-        [self removeHighlight];
+        [self removeHighlight]; //remove highlight on previously selected word
     }
     
     self.highlightedRange = wordRange;
     
+    //highlight selected word
     NSMutableAttributedString* attributedString = [self.label.attributedText mutableCopy];
     [attributedString addAttribute:NSBackgroundColorAttributeName value:[UIColor redColor] range:wordRange];
     self.label.attributedText = attributedString;
@@ -93,6 +97,7 @@
     
     if (self.highlightedRange.location != NSNotFound) {
         
+        //remove highlight from previously selected word
         NSMutableAttributedString* attributedString = [self.label.attributedText mutableCopy];
         [attributedString removeAttribute:NSBackgroundColorAttributeName range:self.highlightedRange];
         self.label.attributedText = attributedString;
